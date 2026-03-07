11fa48780eb39831e1419e5b64dc3377334f39a7
Mahedi Hasan Riad <contact.mahedihasan@gmail.com>
2026-03-07 17:29:14 +0600

    modify login controller

--- src/api/v1/Auth/controller/login.controller.js
+++ src/api/v1/Auth/controller/login.controller.js
@@ -10,23 +10,14 @@ import { verifyCoachingCenter } from "../utils/coachingCenter.js";
 const logInController = asyncHandler(async (req, res) => {
   
   const { email, password } = req.body;
-  const { as = "student" } = req.query;
+
 
   if (!email || !password)
     throw new apiError(400, "Email & Password both are required !!!");
 
-  if (as.trim() === "student") {
-    const {rmPass, accessToken} = await verifyUser(email, password)
-    Responses(res, rmPass, accessToken)
-  }
-  if (as.trim() === "teacher") {
-    const {rmPass, accessToken} = await verifyTeacher(email, password)
-    Responses(res, rmPass, accessToken)
-  }
-  if (as.trim() === "coaching") {
-    const {rmPass, accessToken} = await verifyCoachingCenter(email, password)
-    Responses(res, rmPass, accessToken)
-  }
+  const {user, accessToken} = await verifyUser(email, password)
+  Responses(res, user, accessToken)
+
 });
 
 export { logInController };
--- src/api/v1/Auth/utils/user.js
+++ src/api/v1/Auth/utils/user.js
@@ -5,19 +5,19 @@ import { generateToken } from "../../../../utils/generateAccessToken.js";
 
 export const verifyUser = async (email, password) => {
   try {
-    const user = await User.findOne({ email });
-    if (!user) throw new apiError(404, "User not found !!!");
+    const findUser = await User.findOne({ email });
+    if (!findUser) throw new apiError(404, "User not found !!!");
   
-    const isValidPassword = await user.isPasswordCorrect(password);
+    const isValidPassword = await findUser.isPasswordCorrect(password);
     if (!isValidPassword) throw new apiError(400, "Invalid Password !!!");
   
-    const rmPass = await User.findById(user._id).select("-password")
+    const user = await User.findById(findUser._id).select("-password")
   
-    const { accessToken } = await generateToken(user._id);
+    const { accessToken } = await generateToken(findUser._id);
   
   
     return {
-      rmPass,
+      user,
       accessToken
     }
   } catch (error) {
