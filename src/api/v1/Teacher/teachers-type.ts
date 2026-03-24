71a50fa81e33b3430ed34591a698b8ab748f7105
Mahedi Hasan Riad <contact.mahedihasan@gmail.com>
2026-03-25 01:52:48 +0600

    dist

--- src/api/v1/Teacher/teachers-type.ts
+++ src/api/v1/Teacher/teachers-type.ts
@@ -1,12 +1,12 @@
 export interface TeacherType {
-  name: string;
-  mobile: string;
-  address: string;
-  bio: string;
-  facebook: string;
-  linkedIn: string;
-  education: string;
-  availableDay: string;
-  availableTime: string;
-  experience: string;
+  name: string | undefined;
+  mobile: string | undefined;
+  address: string | undefined;
+  bio: string | undefined;
+  facebook: string | undefined;
+  linkedIn: string | undefined;
+  education: string | undefined;
+  availableDay: string | undefined;
+  availableTime: string | undefined;
+  experience: string | undefined;
 }
